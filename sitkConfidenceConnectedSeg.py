c9971b416acc33f1aa1c2a3ceaf03f6ee12b9457
BrentFoster <bhfoster@ucdavis.edu>
2015-11-09 23:46:34 -0800

    Added a connected component filter which helps to remove leakage areas from the image and is successful for the one test I ran

--- sitkConfidenceConnectedSeg.py
+++ sitkConfidenceConnectedSeg.py
@@ -5,7 +5,7 @@ def ConfidenceConnectedSeg(image, seedPoints):
 
 	print('\033[91m' + "### SEGMENTING ###")
 	print("  ")
-	
+
 	# sitkFloat32
 	image = sitk.Cast(image,sitk.sitkFloat32)
 
@@ -28,7 +28,11 @@ def ConfidenceConnectedSeg(image, seedPoints):
 	erodeFilter = sitk.BinaryErodeImageFilter()
 	erodeFilter.SetKernelRadius(1)
 	fillFilter = sitk.BinaryFillholeImageFilter()
-	
+
+	#Attempt to remove the leakage areas
+	connectedComponentFilter = sitk.ScalarConnectedComponentImageFilter()
+	connectedComponentFilter.SetFullyConnected(True)
+	connectedComponentFilter.SetDistanceThreshold(0.01)
 
 
 
@@ -48,22 +52,47 @@ def ConfidenceConnectedSeg(image, seedPoints):
 		print('\033[94m' + "Current Seed Point: ")
 		print(tempPoint)
 
-		segXImg = sitk.ConfidenceConnected(image, tempPoint, numberOfIterations=10, multiplier=2,	initialNeighborhoodRadius = 5, replaceValue=x+1)
-
+		#its = 25, multiplyer = 2
+		segXImg = sitk.ConfidenceConnected(image, tempPoint, numberOfIterations=25, multiplier=2,	initialNeighborhoodRadius = 5, replaceValue=x+1)
 
 		print('\033[93m' + "Filling Segmentation Holes...")
 		#Apply the filters to the binary image
 		segXImg = dilateFilter.Execute(segXImg, 0, x+1, False)
 		segXImg = fillFilter.Execute(segXImg, True, x+1)
 		segXImg = erodeFilter.Execute(segXImg, 0, x+1, False)
-		
+
+
+
+		print('\033[95m' + "Running Connected Component...")
+
+		#Try to remove leakage areas by first eroding the binary and
+		#get the labels that are still connected to the original seed location
+
+		segXImg = erodeFilter.Execute(segXImg, 0, x+1, False)
+
+		connectedXImg = connectedComponentFilter.Execute(segXImg)
+
+		nda = sitk.GetArrayFromImage(connectedXImg)
+		nda = np.asarray(nda)
+
+		#In numpy an array is indexed in the opposite order (z,y,x)
+		tempPoint = tempPoint[0]
+		val = nda[tempPoint[2]][tempPoint[1]][tempPoint[0]]
+		#TODO Check this
+		nda[nda != x+2] = 0 #Keep only the label that intersects with the seed point
+		nda[nda != 0] = x+1
+
+		segXImg = sitk.GetImageFromArray(nda)
+
+
 
 		segXImg = sitk.Cast(segXImg, segmentation.GetPixelID())
-
+		segXImg.CopyInformation(segmentation)
+		
 		#Combine with the previous segmentations
 		segmentation = segmentation + segXImg
 		print("  ")
-		
+
 
 	segmentation = sitk.Cast(segmentation, sitk.sitkUInt16)
 	originalImage = sitk.Cast(originalImage, sitk.sitkUInt16)
@@ -76,8 +105,8 @@ def ConfidenceConnectedSeg(image, seedPoints):
 		# nda = nda[:,:,:,0] + nda[:,:,:,1] + nda[:,:,:,2]
 		# overlaidSegImage = sitk.GetImageFromArray(nda)
 
-		sitk.Show(overlaidSegImage)	
-	except: 
+		sitk.Show(overlaidSegImage)
+	except:
 		print("Can't show the image with ImageJ! (Probably testing through Linux virtual machine)")
 
 	return segmentation
@@ -87,14 +116,14 @@ if __name__ == '__main__':
 
 	#Create a list for the seed points
 	seedPoints = [[130, 184, 42], [175.987, 90.123, 31.0], [100, 200, 47]]
-	# seedPoints = [[130.1238, 184.98213, 41.123], [175.987, 90.123, 31.0]]
-	
+
+	#Test for leakage
+	seedPoints = [[99, 206, 50]]
+
 	#Read in image
-	image = sitk.ReadImage("Volunteer5_VIBE.hdr")
+	image = sitk.ReadImage("Volunteer5_VIBE_scaled.hdr")
 
 	segmentation = ConfidenceConnectedSeg(image, seedPoints)
-
-
 
 ######################################################################
 	#NOTES#
@@ -120,7 +149,7 @@ if __name__ == '__main__':
 	# print(fastMarchingFilter)
 	# fastMarchingImg = fastMarchingFilter.Execute(image)
 
-	# clear; ./Slicer --no-main-window --python-script /Users/Brent/BoneSegmentation/SlicerModule/BoneSegmentation/BoneSegmentation.py 
-	# /Applications/Slicer.app/Contents/MacOS/Slicer --no-main-window --python-script /Users/Brent/BoneSegmentation/SlicerModule/BoneSegmentation/BoneSegmentation.py 
+	#Run the Slicelet on Brent's MacBook
+	#clear; /Applications/Slicer.app/Contents/MacOS/Slicer --no-main-window --python-script /Users/Brent/BoneSegmentation/BoneSegmentation.py
 
 ######################################################################
