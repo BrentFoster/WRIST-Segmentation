59a9b7274be19f31e0ddcc5fb8422fcd1df89c5d
BrentFoster <bhfoster@ucdavis.edu>
2015-11-11 11:40:27 -0800

    Aded a way to save the user defined initial points in a .txt file

--- sitkConfidenceConnectedSeg.py
+++ sitkConfidenceConnectedSeg.py
@@ -3,18 +3,22 @@ import numpy as np
 
 def ConfidenceConnectedSeg(image, seedPoints):
 
-	print('\033[91m' + "### SEGMENTING ###")
-	print("  ")
+	#Save the user defined points in a .txt for automatimating testing (TODO)
+	text_file = open("PointList.txt", "r+")
+	text_file.readlines()
+	text_file.write("%s\n" % seedPoints)
+	text_file.close()
 
-	# sitkFloat32
-	image = sitk.Cast(image,sitk.sitkFloat32)
 
-	#Copy the orignal image for plotting later
-	originalImage = sitk.Cast(sitk.RescaleIntensity(image), image.GetPixelID())
+	scalingFactor = [2, 2, 1] #X,Y,Z
 
-	#Create empty image to hold the segmentations
-	segmentation = sitk.Image(image.GetSize(), image.GetPixelID())
-	segmentation.CopyInformation(image)
+	### LOAD FILTERS ###
+
+	shrinkFilter = sitk.ShrinkImageFilter()
+	shrinkFilter.SetShrinkFactors(scalingFactor)
+
+	expandFilter = sitk.ExpandImageFilter()
+	expandFilter.SetExpandFactors (scalingFactor)
 
 	###PRE-PROCESSING FILTERS###
 	anisotropicFilter = sitk.CurvatureAnisotropicDiffusionImageFilter()
@@ -29,22 +33,46 @@ def ConfidenceConnectedSeg(image, seedPoints):
 	erodeFilter.SetKernelRadius(1)
 	fillFilter = sitk.BinaryFillholeImageFilter()
 
-	#Attempt to remove the leakage areas
+	#Filter to attempt to remove the leakage areas
 	connectedComponentFilter = sitk.ScalarConnectedComponentImageFilter()
 	connectedComponentFilter.SetFullyConnected(True)
-	connectedComponentFilter.SetDistanceThreshold(0.01)
+	connectedComponentFilter.SetDistanceThreshold(0.01) #Distance=Intensity difference NOT location
 
+	### END FILTERS ###
+
+
+	print('\033[90m' + "Scaling image down...")
+	image = shrinkFilter.Execute(image)
+
+	print('\033[91m' + "### SEGMENTING ###")
+	print("  ")
+
+	# sitkFloat32
+	image = sitk.Cast(image,sitk.sitkFloat32)
+
+	#Copy the orignal image for plotting later
+	originalImage = sitk.Cast(sitk.RescaleIntensity(image), image.GetPixelID())
+
+	#Create empty image to hold the segmentations
+	segmentation = sitk.Image(image.GetSize(), image.GetPixelID())
+	segmentation.CopyInformation(image)
+
+	
 
 
 	###Apply the Anisotropic Filter###
 	print('\033[92m' + "Applying the Anisotropic Filter...")
-	image = anisotropicFilter.Execute(image)
+	# image = anisotropicFilter.Execute(image)
 	print("  ")
+
+
+	# sitk.Show(image)
 
 
 	#Need to round the seedPoints because integers are required for indexing
 	seedPoints = np.array(seedPoints).astype(int)
 	seedPoints = abs(seedPoints)
+	seedPoints = seedPoints/scalingFactor #Scale the points down as well
 
 	for x in range(0, len(seedPoints)):
 
@@ -52,8 +80,8 @@ def ConfidenceConnectedSeg(image, seedPoints):
 		print('\033[94m' + "Current Seed Point: ")
 		print(tempPoint)
 
-		#its = 25, multiplyer = 2
-		segXImg = sitk.ConfidenceConnected(image, tempPoint, numberOfIterations=30, multiplier=2.1,	initialNeighborhoodRadius = 5, replaceValue=x+1)
+		#numberOfIterations = 25, multiplier = 2
+		segXImg = sitk.ConfidenceConnected(image, tempPoint, numberOfIterations=20, multiplier=2, initialNeighborhoodRadius=2, replaceValue=x+1)
 
 		print('\033[93m' + "Filling Segmentation Holes...")
 		#Apply the filters to the binary image
@@ -63,7 +91,11 @@ def ConfidenceConnectedSeg(image, seedPoints):
 		erodeFilter.SetKernelRadius(1)
 		segXImg = erodeFilter.Execute(segXImg, 0, x+1, False)
 
-
+		###TEMP###
+		# segXImg = sitk.Cast(segXImg, segmentation.GetPixelID())
+		# segXImg.CopyInformation(segmentation)
+		# segmentation = segmentation + segXImg
+		###TEMP###
 
 		print('\033[95m' + "Running Connected Component...")
 
@@ -125,21 +157,47 @@ def ConfidenceConnectedSeg(image, seedPoints):
 	except:
 		print("Can't show the image with ImageJ! (Probably testing through Linux virtual machine)")
 
+
+
+	print('\033[90m' + "Scaling image back...")
+	segmentation = expandFilter.Execute(segmentation)
+
+
 	return segmentation
 
 if __name__ == '__main__':
     ### sitkConfidenceConnectedSeg.py executed as script ##
 
 	#Create a list for the seed points
-	seedPoints = [[130, 184, 42], [175.987, 90.123, 31.0], [100, 200, 47]]
+	# seedPoints = [[130, 184, 42], [175.987, 90.123, 31.0], [100, 200, 47]]
 
 	#Test for leakage
-	seedPoints = [[99, 206, 50]]
+	# seedPoints = [[99, 206, 50]]
+	# seedPoints = [[141, 290, 119]]
 
+	#Could load the points from a saved text file (PointList.txt)
+	seedPoints = [(-373.1014570105204, -615.4203026139975, 110.3448307613415), (-204.98551149614664, -670.5623327427121, 110.3448307613415), (-346.2029057282207, -667.8724776144821, 134.48276249038494), (-266.8521794454362, -698.8058115891268, 134.48276249038494), (-209.02029418849162, -679.976825691517, 141.3793144129688), (-242.64348329136638, -623.4898679986875, 141.3793144129688), (-288.37102047127604, -626.1797231269175, 172.41379806459608)]
+	# seedPoints = [(-266.8521794454362, -698.8058115891268, 134.48276249038494)]
+	# seedPoints = [(342, 678, 142)]
+	# seedPoints = [(-204.98551149614664, -670.5623327427121, 110.3448307613415)]
 	#Read in image
-	image = sitk.ReadImage("Volunteer5_VIBE.hdr")
+	# image = sitk.ReadImage("Volunteer5_VIBE.hdr")
+
+	image = sitk.ReadImage("/Users/Brent/Google Drive/Research/Wrist MRI/VIBE/Volunteer2_VIBE.hdr")
+
+
+	#Need to flip the image if loading from the terminal vs. Slicer/Slicelet. Need to fix this
+
+	flipFilter = sitk.FlipImageFilter()
+	flipFilter.SetFlipAxes((False,True,False))
+	image = flipFilter.Execute(image)
 
 	segmentation = ConfidenceConnectedSeg(image, seedPoints)
+
+
+
+
+
 
 ######################################################################
 	#NOTES#
