9c009ec9bdfddfdbef623fb3b69f142826a264a7
BrentFoster <bhfoster@ucdavis.edu>
2015-11-25 08:59:52 -0800

    Removed repeated files from the main folder

--- DICE_test.py
+++
@@ -1,8 +0,0 @@
-import DICE
-
-# '/Users/Brent/Google Drive/Research/Wrist MRI/Ground Truth/Volunteer2_Segmentation.hdr', 
-dice_testing = DICE.DICE()
-
-dice_testing.Execute(\
-	'/Users/Brent/Desktop/Segmentations/Volunteer2_GroundTruth.hdr', \
-	'/Users/Brent/Desktop/Segmentations/Volunteer2_VIBE_segmentation.hdr')
