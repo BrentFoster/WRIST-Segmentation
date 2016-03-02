97969cc115861b9b4c357f74f0081341065384d4
BrentFoster <bhfoster@ucdavis.edu>
2016-03-02 15:59:39 -0800

    Converted the spectral clustering code to a function

--- Automatic Seed Initialization/Automatic_Seed_Initialization.py
+++ Automatic Seed Initialization/Automatic_Seed_Initialization.py
@@ -1,10 +1,134 @@
+#For creating debug file and printing warning to the terminal
+#Allows the user to decide what level to print
+#Allowed levels are CRITICAL, ERROR, WARNING, INFO, DEBUG, NOTSET
+import logging 
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+logging.debug('Starting automatic seed initialization...')
+
+
+import SimpleITK as sitk
+import numpy as np
+
+''' Load images '''
+MRI_Filename = '/Users/Brent/Google Drive/Research/MRI Wrist Images/CMC OA/Volunteer 3/VIBE/Volunteer3_VIBE_we.hdr'
+# MRI_Filename = '/Users/Brent/Google Drive/Research/MRI Wrist Images/CMC OA/Volunteer 2/VIBE/Volunteer2_VIBE_we.hdr'
+MRI = sitk.ReadImage(MRI_Filename)
+MRI = sitk.Cast(MRI, sitk.sitkUInt16) # Cast to 16 bit image 
+
+''' Roughly segment the hand boundary (use automatic thresholding in SimpleITK)
+	Could either hard code the thresholds based on VIBE or use a ITK method '''
+
+# Initialize various ITK filters
+otsuFilter = sitk.OtsuMultipleThresholdsImageFilter()
+otsuFilter.SetNumberOfHistogramBins(256)
+
+kernelRadius = [2,2,2]
+medianFilter = sitk.BinaryMedianImageFilter()
+medianFilter.SetRadius(kernelRadius)
+
+kernelRadius = [5,5,5]
+dilateFilter = sitk.BinaryDilateImageFilter()
+dilateFilter.SetBackgroundValue(0)
+dilateFilter.SetForegroundValue(1)
+dilateFilter.SetKernelRadius(kernelRadius)
+
+fillFilter = sitk.BinaryFillholeImageFilter()
+fillFilter.SetForegroundValue(1)
+fillFilter.FullyConnectedOn()
+
+run_pipeline = False
+if run_pipeline == True:
+	# Run pipeline
+	logging.debug('Running Otsu thresholding')
+	segHand = otsuFilter.Execute(MRI)
+	logging.debug('Running median filter')
+	segHand = medianFilter.Execute(segHand)
+	logging.debug('Running median filter')
+	segHand = medianFilter.Execute(segHand)
+	logging.debug('Running median filter')
+	segHand = medianFilter.Execute(segHand)
+	logging.debug('Running dilation filter')
+	segHand = dilateFilter.Execute(segHand)
+	logging.debug('Running dilation filter')
+	segHand = dilateFilter.Execute(segHand)
+	logging.debug('Running binary fill filter')
+	segHand = fillFilter.Execute(segHand)
+
+	# sitk.Show(segHand)
 
 
 
-# Roughly segment the hand boundary (use automatic thresholding in SimpleITK)
-# Could either hard code the thresholds based on VIBE or use a ITK method
+	# Save this image using ITK
+	imageWriter = sitk.ImageFileWriter()
+	imageWriter.Execute(segHand, 'segHand_volunteer_3.nii', True)
+
+import BrentVTK
+movingFilename = 'segHand_volunteer_3_carpals.nii'
+fixedFilename  = 'HandModel_volunteer_2_carpals.nii'
+labels = (1,0)
+
+
+BrentVTK.main(movingFilename, fixedFilename, labels, show_rendering=True, calculateDice=False)
+
+
+# Overlap with model for goodness of fit evaluation
+transformedImg = sitk.ReadImage('vtk_Transformed_Img.nii')
+transformedImg = sitk.Cast(transformedImg, sitk.sitkUInt16) 
+
+hand_model_img = sitk.ReadImage('HandModel_volunteer_2_carpals.nii')
+hand_model_img = sitk.Cast(hand_model_img, sitk.sitkUInt16) 
+
+
+
+
+ndaTransformImg = np.asarray(sitk.GetArrayFromImage(transformedImg))
+nda_hand_model_img = np.asarray(sitk.GetArrayFromImage(hand_model_img))
+
+
+ndaTransformImg = ndaTransformImg + nda_hand_model_img
+
+overlapImg = sitk.GetImageFromArray(ndaTransformImg)
+overlapImg.CopyInformation(MRI)
+
+sitk.Show(overlapImg)
+
+
+
+# segHand = sitk.ReadImage('binary_hand_volunteer_1.nii')
+
+# ndaMRI  = np.asarray(sitk.GetArrayFromImage(MRI))
+# ndaHand = np.asarray(sitk.GetArrayFromImage(segHand))
+
+# ndaSegMRI = ndaMRI * ndaHand
+
+# segMRI = sitk.GetImageFromArray(ndaSegMRI)
+# segMRI.CopyInformation(MRI)
+
+# otsuFilter.SetNumberOfHistogramBins(128)
+
+# logging.debug('Running Otsu thresholding')
+# segMRI = otsuFilter.Execute(segMRI)
+
+# sitk.Show(segMRI)
+
+
+# medianFilter.SetRadius([1,1,1])
+# logging.debug('Running median filter')
+# segMRI = medianFilter.Execute(segMRI)
+# segMRI = medianFilter.Execute(segMRI)
+
+# logging.debug('Running dilation filter')
+# dilateFilter.SetKernelRadius([2,2,2])
+# segMRI = dilateFilter.Execute(segMRI)
+
+# logging.debug('Running binary fill filter')
+# segMRI = fillFilter.Execute(segMRI)
+
+# logging.debug('Done!')
+# sitk.Show(segMRI)
 
 # Use ICP to register the hand surface to the hand model
+
 
 # Apply the resulting transformation to a set of points in the hand model
 # One point for each bone
@@ -18,3 +142,4 @@
 
 # If leakage is detected, apply spectral clustering to remove leakage areas
 
+# Do everything in parallel to lower computation time (up to 8x faster)
---
+++ Spectral Clustering/Spectral_Clustering.py
@@ -0,0 +1,88 @@
+import sys
+# from __main__ import vtk, qt, ctk, slicer
+
+from sklearn.feature_extraction import image
+from sklearn.cluster import spectral_clustering
+import SimpleITK as sitk
+import numpy as np
+
+#Allowed levels are CRITICAL, ERROR, WARNING, INFO, DEBUG, NOTSET
+import logging 
+
+def load_image(Binary_Filename, ScalingFactor=[4,4,4]):
+	''' Load the input image and downsample (to speed up computation) '''
+
+	logging.info('Loading image')
+	img = sitk.ReadImage(Binary_Filename)
+	
+	logging.info('Down sampling image')
+	shrinkFilter = sitk.ShrinkImageFilter()
+	shrinkFilter.SetShrinkFactors(ScalingFactor)
+
+	img = shrinkFilter.Execute(img)
+
+	return img
+
+
+def main(sitkImg, label=1, ScalingFactor=[4,4,4]):
+	''' Use spectral clustering on a binary image to remove the leakage areas '''
+
+	logging.info('Converting to numpy array')
+	
+	img = sitk.GetArrayFromImage(sitkImg)
+
+	img[img != label] = 0
+	img[img != 0] = 1
+
+	mask = img
+	mask[mask != 0] = 1
+
+	mask = mask.astype(bool)
+	img = img.astype(int)
+
+	logging.info('Creating graph from image')
+
+	# Convert the image into a graph with the value of the gradient on the edges.
+	graph = image.img_to_graph(img, mask=mask)
+
+	# Take a decreasing function of the gradient
+	graph.data = np.exp(-graph.data / graph.data.std())
+
+	logging.info('Running spectral clustering')
+	labels = spectral_clustering(graph, n_clusters = 2, eigen_solver='arpack')
+
+	npLabel = -np.ones(mask.shape)
+	npLabel[mask] = labels
+
+
+	# Convert back to SimpleITK image type and upsample back to original size
+	expandFilter = sitk.ExpandImageFilter()
+	expandFilter.SetExpandFactors(ScalingFactor)
+
+	Labelimg = sitk.Cast(sitk.GetImageFromArray(npLabel), sitkImg.GetPixelID())
+	Labelimg = expandFilter.Execute(Labelimg)
+
+
+	return Labelimg
+
+
+if __name__ == "__main__":
+	logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+	
+	print sys.argv
+
+	if len(sys.argv) > 1:
+		Binary_Filename = sys.argv[1]
+	else:
+		Binary_Filename = 'Volunteer1_GroundTruth.hdr'
+
+	ScalingFactor = [9,9,9]
+
+
+
+	img = load_image(Binary_Filename, ScalingFactor)
+	sc_img = main(img, label=1, ScalingFactor=ScalingFactor)
+
+	sitk.Show(sc_img)
+
+	logging.info('Done!')
